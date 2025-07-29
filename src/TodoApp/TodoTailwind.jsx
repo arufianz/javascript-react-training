9d9d5e8e87c6dc8501471561c4134d43183983ba
arufianz <alfianwashere@gmail.com>
2025-07-29 18:41:56 +0700

    Create Todo list App

--- src/TodoApp/TodoTailwind.jsx
+++ src/TodoApp/TodoTailwind.jsx
@@ -1,12 +1,13 @@
 import { useState} from 'react';
-import style from './TodoTailwind.module.css'
 function TodoTailwind(){
-    const [task, setTask] = useState([]);
+    const [task, setTask] = useState(["Test", "Test2", "Test3 lorem ipsum dolor sit amet"]);
     
     function AddTask(e){
         const newTask = document.getElementById('taskInput').value;
-        if (newTask == '')
+        if (newTask == ''){
+            alert('Masukkan Tugas');
             return;
+        }
         document.getElementById('taskInput').value =  '';
         setTask(t => [...t, newTask]);
     }
@@ -14,18 +15,43 @@ function TodoTailwind(){
     function DeleteTask(e){
         setTask(task.filter((_, i) => i !== e));
     }
+    function MoveTaskUp(index) {
+        const updatedTasks = [...task]; 
+        if (index > 0) {
+            const temp = updatedTasks[index]; 
+            updatedTasks[index] = updatedTasks[index - 1]; 
+            updatedTasks[index - 1] = temp;
+        }
+        setTask(updatedTasks);
+    }
+    function MoveTaskDown(index) {
+        const updatedTasks = [...task]; 
+        if (index < task.length - 1) {
+            const temp = updatedTasks[index]; 
+            updatedTasks[index] = updatedTasks[index + 1]; 
+            updatedTasks[index + 1] = temp;
+        }
+        setTask(updatedTasks);
+    }
+
     return(
-        <div className='p-4 text-center bg-green-500 m-4 rounded-xl shadow-lg'>
-            <h1 className='text-2xl color-white '> Todo App</h1>
-            <input type="text" id='taskInput' 
+        <div className='my-8 mx-auto w-[400px] p-4 justify-self-center text-center bg-green-500 rounded-xl shadow-lg flex flex-col shadow-black'>
+            <h1 className='text-2xl color-white m-4'> To-Do List</h1>
+            <input className='px-1 py-1 mx-2 rounded-lg bg-white shadow-md' type="text" id='taskInput' 
             placeholder='Masukkan Tugas' onKeyDown={(e) => e.key === 'Enter' && AddTask()}
             />
-            <button onClick={AddTask}>Tambah</button>
-            <hr />
+            <button className='p-1 px-2 my-3 mx-auto w-[30%] rounded-lg bg-yellow-400 shadow-md hover:bg-yellow-500' onClick={AddTask}>Tambah</button>
+            <div className='m-auto border-solid my-4 bg-white h-2 rounded w-[90%] shadow-md'/>
             <ul>
                 {task.map((task, index) =>
-                <li key={index} onClick={() => DeleteTask(index)}>
-                    {task}</li>)}
+                <li className='p-1 m-2 rounded-lg bg-white shadow-md flex'>
+                    <p className='text-left'>{task}</p>
+                    <div className='flex ml-auto h-8 my-auto'>
+                        <button className='px-1 py-1 ml-2 rounded-lg bg-blue-600 hover:bg-blue-500' key={index} onClick={() => MoveTaskUp(index)}>👆</button>
+                        <button className='px-1 py-1 ml-2 rounded-lg bg-blue-600 hover:bg-blue-500' key={index} onClick={() => MoveTaskDown(index)}>👇</button>
+                        <button className='px-2 py-1 ml-2 rounded-lg bg-red-400 hover:bg-red-500' key={index} onClick={() => DeleteTask(index)}>Delete</button>
+                    </div>
+                </li>)}
             </ul>
         </div>
     );
